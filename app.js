b336a123b6169a52a33f8e9cab5dba9b7cac0533
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2023-11-03 18:10:48 +0100

    Serving static files

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`));
 app.use(function (request, response, next) {
   request.requestedTime = new Date().toISOString();
   next();
 });
+
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 module.exports = app;
