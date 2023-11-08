64a3a5819561a479603b340ad0b9e4b9dcef1f41
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2023-11-08 19:25:06 +0100

    Creating the Tour model

--- server.js
+++ server.js
@@ -3,22 +3,7 @@ const mongoose = require('mongoose');
 dotenv.config({ path: `${__dirname}/config.env` });
 
 const app = require('./app');
-/*
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD,
-);
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then((conn) => {
-    console.log(conn.connections);
-    console.log('DB connection successful!');
-  });
-  */
+
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD,
@@ -26,9 +11,26 @@ const DB = process.env.DATABASE.replace(
 mongoose.connect(DB);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection failed:'));
-db.once('open', function () {
+db.once('open', () => {
   console.log('Connected to database successfully');
 });
+//Creating a Schema for our model, to describe the data and also validate it
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+//Creating our model, the first letter has to be in uppercase
+const Tour = mongoose.model('Tour', tourSchema);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
